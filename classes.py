b7fd4e4a22be2dff4dfea8cb7e13f0982625ba35
entegral <e2robby@gmail.com>
2016-02-23 13:32:23 -0800

    third python commit

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 class Home():
-    def __init__(self, address, temperature, occupied):
+    def __init__(self, residents, address, temperature, occupied):
+        self.residents = residents
         self.address = address
         self.temperature = temperature
         self.occupied = occupied
@@ -8,12 +9,17 @@ class Home():
         return self.occupied
 
 class Room(Home):
-    def __init__(self, address, temperature, occupied, doors, windows):
-        Home.__init__(self, address, temperature, occupied)
+    def __init__(self, residents, address, temperature, occupied, doors, windows):
+        Home.__init__(self, residents, address, temperature, occupied)
         self.doors = doors
         self.windows = windows
 
 class Opening(Room):
-    def __init__(self, address, temperature, occupied, doors, windows, status):
-        Room.__init__(self, address, temperature, occupied, doors, windows)
+    def __init__(self, residents, address, temperature, occupied, doors, windows, status):
+        Room.__init__(self, residents, address, temperature, occupied, doors, windows)
         self.status = status
+
+my_room = Room(['Robby'],'702 SW Cheltenham St', 70, True, 1, 3)
+print my_room.residents
+print my_room.windows
+print my_room.occupied
