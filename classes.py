ba13623d8f5c9c56710d8f19c7ed5c95bd7e671d
entegral <e2robby@gmail.com>
2016-02-23 14:46:19 -0800

    first commit from within pycharm

--- classes.py
+++ classes.py
@@ -5,8 +5,12 @@ class Home():
         self.temperature = temperature
         self.occupied = occupied
 
-    def isOccupied(self, occupied):
-        return self.occupied
+    def isoccupied(self):
+        if self.occupied is True:
+            return str(self.occupied) + ", " + str(self.address) + " is occupied by " + str(self.residents) + "."
+        else:
+            return str(self.occupied) + ", " + str(self.address) + " is not occupied at this time."
+
 
 class Room(Home):
     def __init__(self, residents, address, temperature, occupied, doors, windows):
@@ -14,12 +18,13 @@ class Room(Home):
         self.doors = doors
         self.windows = windows
 
+
 class Opening(Room):
     def __init__(self, residents, address, temperature, occupied, doors, windows, status):
         Room.__init__(self, residents, address, temperature, occupied, doors, windows)
         self.status = status
 
-my_room = Room(['Robby'],'702 SW Cheltenham St', 70, True, 1, 3)
+my_room = Room(['Robby'], '702 SW Cheltenham St', 70, False, 1, 3)
 print my_room.residents
 print my_room.windows
-print my_room.occupied
+print my_room.isoccupied()
