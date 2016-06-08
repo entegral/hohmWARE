26b389fd0f6f863e24c0661de094a3342877c1ac
entegral <e2robby@gmail.com>
2016-06-07 22:54:45 -0700

    added gpio monitor;

---
+++ resident_view.py
@@ -0,0 +1,29 @@
+
+def printAllResidents(residents):
+	print ("\n-------------------------------------------")
+	for resident in residents:
+		print ("\nResident Name: " + resident.name)
+		print ("Resident Email: " + resident.email)
+		print ("Resident Phone: " + resident.phone)
+		print ("Resident Mac Address: " + resident.mac + "\n")
+	print ("-------------------------------------------")
+
+def printAllResident(resident):
+    print ("\nResident Name: " + resident.name)
+    print ("Resident Email: " + resident.email)
+    print ("Resident Phone: " + resident.phone)
+    print ("Resident Mac Address: " + resident.mac + "\n")
+
+
+def printResidentName(resident):
+	print ("\nResident Name: " + resident.name)
+	
+def printResidentEmail(resident):
+    print ("Resident Email: " + resident.email)
+
+def printResidentPhone(resident):
+	print ("Resident Phone: " + resident.phone)
+
+def printResidentMac(resident):
+	print ("Resident Mac Address: " + resident.mac + "\n")
+
--- zone_view.py
+++ zone_view.py
@@ -1,37 +1,16 @@
 
 
 
-def printZones(zones):
+def printZoneNames(zones):
+	print ("-------------------------------------------")
 	for zone in zones:
 		print("Zone Name: " + zone.name)
+		print("Zone Channel: " + str(zone.channel) + '\n')
+	print ("-------------------------------------------")
 
-def printZone(zone):
+
+def printZoneName(zone):
+	print ("-------------------------------------------")
 	print("Zone Name: " + zone.name)
-
-
-def printAllResidentsData(residents):
-	for resident in residents:
-		print ("\nResident Name: " + resident.name)
-		print ("Resident Email: " + resident.email)
-		print ("Resident Phone: " + resident.phone)
-		print ("Resident Mac Address: " + resident.mac + "\n")
-
-def printAllResidentData(resident):
-    print ("\nResident Name: " + resident.name)
-    print ("Resident Email: " + resident.email)
-    print ("Resident Phone: " + resident.phone)
-    print ("Resident Mac Address: " + resident.mac + "\n")
-
-
-def printResidentName(resident):
-	print ("\nResident Name: " + resident.name)
-	
-def printResidentEmail(resident):
-    print ("Resident Email: " + resident.email)
-
-def printResidentPhone(resident):
-	print ("Resident Phone: " + resident.phone)
-
-def printResidentMac(resident):
-	print ("Resident Mac Address: " + resident.mac + "\n")
+	print ("-------------------------------------------")
 
