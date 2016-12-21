ff9f2e9a74dc1e6a90db119945a416acad24561f
Rob <e2robby@gmail.com>
2016-12-21 14:10:28 -0800

    added local ip ping

---
+++ ping.py
@@ -0,0 +1,19 @@
+
+
+def ping(host):
+    """
+    Returns True if host responds to a ping request
+    """
+    import os, platform
+
+    # Ping parameters as function of OS
+    if  platform.system().lower()=="windows":
+        ping_str = "-n"
+    else:
+        ping_str = "-c"
+
+    # Ping
+    return os.system("ping " + ping_str + " 5 " + host) == 0
+
+
+ping("192.168.1.142")
